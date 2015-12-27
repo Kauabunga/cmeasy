d59788d735abf2291e4ffa6be8563cfd7c263857
Carson Bruce <carson.bruce@solnet.co.nz>
2015-12-27 15:37:24 +1300

    reduced test coverage globals

--- Gruntfile.js
+++ Gruntfile.js
@@ -500,10 +500,10 @@ module.exports = function (grunt) {
         options: {
           coverageFolder: 'coverage/**',
           check: {
-            lines: 80,
-            statements: 80,
-            branches: 80,
-            functions: 80
+            lines: 50,
+            statements: 50,
+            branches: 50,
+            functions: 50
           }
         }
       }
