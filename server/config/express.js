7bcec1d5885ea42471e8ac6ca2600264fa864872
Carson Bruce <carson.bruce@solnet.co.nz>
2016-01-08 14:52:28 +1300

    Try catch connect-livereload uneeded plugin

--- server/config/express.js
+++ server/config/express.js
@@ -79,7 +79,8 @@ function coreExpress(app){
 
   /* istanbul ignore if */
   if ('development' === env) {
-    app.use(require('connect-livereload')());
+    try { app.use(require('connect-livereload')()); }
+    catch(err) { console.error('Error loading connent-livereload module | app.env = ', env); }
   }
 }
 
