7f1fee12f65cc8ac126f4fb4387de7d135c95c26
Carson Bruce <carson.bruce@solnet.co.nz>
2016-01-13 15:33:38 +1300

    missing client list component

---
+++ client/components/admin-content-list/admin-content-list.directive.js
@@ -0,0 +1,78 @@
+'use strict';
+
+angular.module('cmeasyApp')
+  .directive('adminContentList', function ($state, $log, $http, $q, $stateParams, Admin, $timeout, Toast, appConfig, Util) {
+    return {
+      templateUrl: 'components/admin-content-list/admin-content-list.html',
+      restrict: 'E',
+      scope: {},
+      link: function (scope, element, attrs) {
+
+        return init();
+
+        /**
+         *
+         */
+        function init(){
+
+          scope.getModelLabel = getModelLabel;
+          scope.openModel = openModel;
+
+          return Admin.getModels()
+            .then(function(models){
+              scope.models = models;
+            });
+        }
+
+
+        /**
+         *
+         * @param model
+         * @returns {*}
+         */
+        function getModelLabel(model){
+          return Util.getPrettyLabel(getModelId(model));
+        }
+
+        /**
+         *
+         * @param model
+         */
+        function openModel(model){
+          if(isSingleton(model)){
+            $state.go(appConfig.state.item, {
+              itemType: getModelId(model),
+              itemId: getModelId(model)
+            });
+          }
+          else {
+            $state.go(appConfig.state.list, {
+              itemType: getModelId(model)
+            });
+          }
+        }
+
+
+        /**
+         *
+         * @param model
+         * @returns {*}
+         */
+        function getModelId(model){
+          return _.get(model, 'meta.' + appConfig.itemIdKey);
+        }
+
+
+
+        /**
+         *
+         * @param model
+         * @returns {*}
+         */
+        function isSingleton(model){
+          return _.get(model, 'meta.singleton');
+        }
+
+      }
+    };
+  });
