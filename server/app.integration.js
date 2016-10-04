1e63d6cc4b07ae73073a8ccc8917d405637ab2b3
Michael Robinson <michael.robinson@solnet.co.nz>
2016-10-04 16:31:40 +1300

    Don't use code for tests

--- server/app.integration.js
+++ server/app.integration.js
@@ -1,5 +1,5 @@
-const expect = require('code').expect;
 const mongoose = require('mongoose');
+const expect = require('chai').expect;
 
 import app from './app';
 
@@ -8,8 +8,8 @@ describe('app', function () {
   it('should return a promise that resolves with a cmeasy instance', function (done) {
     app()
       .then(function (cmeasy) {
-        expect(cmeasy).to.not.be.null();
-        expect(cmeasy.getModel).to.be.a.function();
+        expect(cmeasy).to.not.equal(null);
+        expect(cmeasy.getModel).to.be.an('function');
         done();
       })
   });
