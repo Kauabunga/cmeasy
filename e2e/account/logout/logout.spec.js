7f5f3ae4dcb2dea7d6f001c176c7e97a2cb13953
Michael Robinson <michael.robinson@solnet.co.nz>
2016-10-05 15:14:46 +1300

    Lean on defaults in initialUsers for e2e tests

--- e2e/account/logout/logout.spec.js
+++ e2e/account/logout/logout.spec.js
@@ -1,34 +1,18 @@
 'use strict';
 
-var config = browser.params;
-var UserModel = require(config.serverConfig.root + '/server/api/user/user.model');
-
+const config = browser.params;
 describe('Logout View', function() {
-  var login = function(user) {
-    let promise = browser.get(config.baseUrl + '/login');
-    require('../login/login.po').login(user);
-    return promise;
-  };
 
-  var testUser = {
+  const testUser = {
     name: 'Test User',
-    email: 'test@example.com',
+    email: 'test@test.com',
     password: 'test'
   };
 
-  beforeEach(function() {
-    return UserModel
-      .remove()
-      .then(function() {
-        return UserModel.create(testUser);
-      })
-      .then(function() {
-        return login(testUser);
-      });
-  });
-
-  after(function() {
-    return UserModel.remove();
+  before(() => {
+    let promise = browser.get(config.baseUrl + '/login');
+    require('../login/login.po').login(testUser);
+    return promise;
   });
 
   describe('with local auth', function() {
