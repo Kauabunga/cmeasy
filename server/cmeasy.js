be037774e841b3b1b93340562bbbdd5a7d76214c
Michael Robinson <michael.robinson@solnet.co.nz>
2016-10-05 14:54:06 +1300

    Implemented json-based user seeding

--- server/cmeasy.js
+++ server/cmeasy.js
@@ -1,13 +1,8 @@
-/**
- * Cmeasy
- */
-
 'use strict';
 
 import _ from 'lodash';
-
 import Promise from 'bluebird';
-var mongoose = Promise.promisifyAll(require('mongoose'));
+const mongoose = require('mongoose');
 mongoose.Promise = Promise;
 
 import express from 'express';
@@ -22,27 +17,38 @@ import createSchemaController from './components/cmeasy/cmeasy.controller.schema
 import createFormlyController from './components/cmeasy/cmeasy.controller.formly';
 import createCrudController from './api/generated/index';
 
+const debug = require('debug')('cmeasy:cmeasy');
+
+import User from '../server/api/user/user.model';
+
 const CMEASY_ID = '_cmeasyId';
 const CMEASY_INSTANCE_ID = '_cmeasyInstanceId';
 
-/**
- *
- */
 export default class Cmeasy {
 
   constructor(options = {}) {
-    this.name = options.name || 'Cmeasy';
-    this.options = new CmeasyOptions(options);
-    this.models = [];
+  }
 
-    this._schema = createSchema(this.getNamespace(), this.getMongoose(), this);
-    this._schemaController = createSchemaController(this);
-    this._schemaFormly = createFormlyController(this.getSchemaMetaId(), this.getSchemaController());
-    this._schemaCrud = createCrudController(this.getSchemaController(), this.getSchemaFormly());
+  static create(options) {
+    const instance = new Cmeasy(options);
+    debug('Initialising');
+    instance.name = options.name || 'Cmeasy';
+    instance.options = new CmeasyOptions(options);
+    instance.models = [];
 
-    return Promise.all(this.generateModels(options.models))
-      .then((models) => {
-        return (this);
+    instance._schema = createSchema(instance.getNamespace(), instance.getMongoose(), instance);
+    instance._schemaController = createSchemaController(instance);
+    instance._schemaFormly = createFormlyController(instance.getSchemaMetaId(), instance.getSchemaController());
+    instance._schemaCrud = createCrudController(instance.getSchemaController(), instance.getSchemaFormly());
+
+    debug('Generating models');
+    return Promise.all(instance.generateModels(options.models))
+      .then(() => {
+        return instance.seedDatabase();
+      })
+      .then(() => {
+        debug('Initialisation complete');
+        return instance;
       });
   }
 
@@ -94,7 +100,7 @@ export default class Cmeasy {
   getDefaultDefinition(definition) {
     var index = 0;
 
-    //Add a default order to the items
+    // Add a default order to the items
     _(definition).map(function(definitionItem) {
       definitionItem.order = definitionItem.order || (++index) * 10;
     }).value();
@@ -165,30 +171,60 @@ export default class Cmeasy {
     return CMEASY_INSTANCE_ID;
   }
 
+  seedDatabase() {
+    const debug = require('debug')('cmeasy:cmeasy:seedDatabase');
+    debug('Seeding database');
+    if (!config.initialUsers) {
+      debug('options.initialUsers is null or undefined, using defaults');
+      config.initialUsers = {
+        clean: true,
+        data: [
+          {
+            name: 'Test User',
+            email: 'test@test.com',
+            password: 'test'
+          },
+          {
+            name: 'Admin',
+            role: 'admin',
+            email: 'admin@admin.com',
+            password: 'admin'
+          }
+        ]
+      }
+    }
+
+    let seedActions = Promise.resolve();
+    if (config.initialUsers.clean) {
+      debug('Erasing all users from db');
+      seedActions = User.find({}).remove();
+    }
+
+    return seedActions
+      .then(() => User.create(config.initialUsers.data))
+      .catch((error) => {
+        debug('Seed database failed');
+        console.error(error);
+        process.exit(1);
+      });
+  }
+
 }
 
-
-/**
- * TODO
- */
 class CmeasyOptions {
 
   constructor(options) {
-
     this.options = options;
 
     if (!this.isUserDefinedMongoose()) {
       this.connectToMongo();
     }
+
     if (!this.isUserDefinedEnvironment()) {
       this.options.environment = 'production';
     }
-
-    this.seedMongo();
-
   }
 
-  //TODO config urls
   connectToMongo() {
     if (!mongoose.connection.readyState) {
       mongoose.connect(config.mongo.uri, config.mongo.options);
@@ -199,15 +235,8 @@ class CmeasyOptions {
     }
   }
 
-  // TODO always seed - i.e. fix tests to handle initial seed
-  seedMongo() {
-    if (config.seedDB) {
-      require('./config/seed')();
-    }
-  }
-
   getMongoose() {
-    return (this.options.mongoose && Promise.promisifyAll(this.options.mongoose)) || Promise.promisifyAll(mongoose);
+    return this.options.mongoose || mongoose;
   }
 
   isUserDefinedEnvironment() {
@@ -222,6 +251,10 @@ class CmeasyOptions {
     return !!this.options.express;
   }
 
+  isUserDefinedInitialUsers() {
+    return Boolean(this.options.initialUsers);
+  }
+
   getExpress() {
     return this.options.express || express;
   }
@@ -230,16 +263,14 @@ class CmeasyOptions {
     return this.options.rootRoute || 'admin';
   }
 
+  getUserModel() {
+    return User;
+  }
 }
 
-
-/**
- *
- */
 class CmeasyModel {
 
   constructor(cmeasy, model) {
-
     this.meta = model.meta;
     this.definition = model.definition;
 
@@ -249,13 +280,11 @@ class CmeasyModel {
     this._modelController = createModelController(this, cmeasy.getSchemaController());
     this._modelFormly = createFormlyController(this.getId(), cmeasy.getSchemaController());
     this._modelCrud = createCrudController(this.getModelController(), this.getModelFormly());
-
   }
 
   getModel() {
     return this._model;
   }
-
 
   getModelFormly() {
     return this._modelFormly;
@@ -280,5 +309,4 @@ class CmeasyModel {
   getInstanceKey() {
     return CMEASY_INSTANCE_ID;
   }
-
 }
